d144cae5ca7828478a1b9c82ad9106085b0388f1
cong meng <mcpacino@gmail.com>
2021-08-02 18:05:42 +1200

    feat(edge): provide a way to re associate an edge endpoint to a new edge agent EE-424 (#566)
    * feat: EE-424 Provide a way to re-associate an Edge endpoint to a new Edge agent
    
    * feat: EE-424 delete the tunnel after de-associate an Edge endpoint
    
    * feat: EE-424 cleanup code
    
    Co-authored-by: Simon Meng <simon.meng@portainer.io>

--- app/portainer/views/endpoints/edit/endpointController.js
+++ app/portainer/views/endpoints/edit/endpointController.js
@@ -21,7 +21,8 @@ angular
     Notifications,
     Authentication,
     SettingsService,
-    StateManager
+    StateManager,
+    ModalService
   ) {
     $scope.state = {
       uploadInProgress: false,
@@ -121,6 +122,29 @@ angular
         $scope.endpoint.TagIds = $scope.endpoint.TagIds.concat(tag.Id);
       } catch (err) {
         Notifications.error('Failue', err, 'Unable to create tag');
+      }
+    }
+
+    $scope.onDeassociateEndpoint = async function () {
+      ModalService.confirmDeassociate((confirmed) => {
+        if (confirmed) {
+          deassociateEndpoint();
+        }
+      });
+    };
+
+    async function deassociateEndpoint() {
+      var endpoint = $scope.endpoint;
+
+      try {
+        $scope.state.actionInProgress = true;
+        await EndpointService.deassociateEndpoint(endpoint.Id);
+        Notifications.success('Endpoint de-associated', $scope.endpoint.Name);
+        $state.reload();
+      } catch (err) {
+        Notifications.error('Failure', err, 'Unable to de-associate endpoint');
+      } finally {
+        $scope.state.actionInProgress = false;
       }
     }
 
