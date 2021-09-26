68934ffab8a41ba089337a57064728743cb0aaab
Richard Wei <54336863+WaysonWei@users.noreply.github.com>
2021-09-27 10:09:17 +1300

    fix error message not last long enough for user to copy error (#787)

--- app/config.js
+++ app/config.js
@@ -61,7 +61,12 @@ angular.module('portainer').config([
       },
     ]);
 
-    toastr.options.timeOut = 3000;
+    toastr.options = {
+      timeOut: 3000,
+      closeButton: true,
+      progressBar: true,
+      tapToDismiss: false,
+    };
 
     Terminal.applyAddon(fit);
 
