0306fb9be98400200f5924392f99446c5aa1e6d8
Marcelo Rydel <marcelorydel26@gmail.com>
2021-10-25 18:18:48 -0300

    fix(git-stacks): UI bugs when using a PAT when deploying from Git [EE-1731] (#893)

--- api/http/handler/stacks/stack_update_git.go
+++ api/http/handler/stacks/stack_update_git.go
@@ -148,6 +148,10 @@ func (handler *Handler) stackUpdateGit(w http.ResponseWriter, r *http.Request) *
 			Username: payload.RepositoryUsername,
 			Password: password,
 		}
+		_, err = handler.GitService.LatestCommitID(stack.GitConfig.URL, stack.GitConfig.ReferenceName, stack.GitConfig.Authentication.Username, stack.GitConfig.Authentication.Password)
+		if err != nil {
+			return &httperror.HandlerError{StatusCode: http.StatusInternalServerError, Message: "Unable to fetch git repository", Err: err}
+		}
 	} else {
 		stack.GitConfig.Authentication = nil
 	}
