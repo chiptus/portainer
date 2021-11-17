db2307c74adc8c9d43b5e2ef1fd46e04a77f4c4d
Prabhat Khera <91852476+prabhat-org@users.noreply.github.com>
2021-11-18 10:58:19 +1300

    feat(images): allow tags when importing docker image EE-1737 (#971)

--- app/docker/views/images/import/importImageController.js
+++ app/docker/views/images/import/importImageController.js
@@ -29,36 +29,44 @@ angular.module('portainer.docker').controller('ImportImageController', [
       $scope.state.pullImageValidity = validity;
     }
 
-    function tagImage(id) {
+    async function tagImage(id) {
       const registryModel = $scope.formValues.RegistryModel;
       if (registryModel.Image) {
         const image = ImageHelper.createImageConfigForContainer(registryModel);
-        ImageService.tagImage(id, image.fromImage);
+        try {
+          await ImageService.tagImage(id, image.fromImage);
+        } catch (err) {
+          Notifications.error('Failure', err, 'Unable to tag image');
+        }
       }
     }
 
-    $scope.uploadImage = function () {
+    $scope.uploadImage = async function () {
       $scope.state.actionInProgress = true;
 
       var nodeName = $scope.formValues.NodeName;
       HttpRequestHelper.setPortainerAgentTargetHeader(nodeName);
       var file = $scope.formValues.UploadFile;
-      ImageService.uploadImage(file)
-        .then(async function success(res) {
+      try {
+        const { data } = await ImageService.uploadImage(file);
+        if (data.error) {
+          Notifications.error('Failure', data.error, 'Unable to upload image');
+        } else if (data.stream) {
           var regex = /Loaded.*?: (.*?)\n$/g;
-          var imageIds = regex.exec(res.data.stream);
+          var imageIds = regex.exec(data.stream);
           if (imageIds && imageIds.length == 2) {
             await tagImage(imageIds[1]);
             $state.go('docker.images.image', { id: imageIds[1] }, { reload: true });
           }
           Notifications.success('Images successfully uploaded');
-        })
-        .catch(function error(err) {
-          Notifications.error('Failure', err, 'Unable to upload image');
-        })
-        .finally(function final() {
-          $scope.state.actionInProgress = false;
-        });
+        } else {
+          Notifications.success('The uploaded tar file contained multiple images. The provided tag therefore has been ignored.');
+        }
+      } catch (err) {
+        Notifications.error('Failure', err, 'Unable to upload image');
+      } finally {
+        $scope.state.actionInProgress = false;
+      }
     };
   },
 ]);
