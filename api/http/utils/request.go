7f315e465db6477f22ba3cfdc0ea300486c79f33
sunportainer <93502624+sunportainer@users.noreply.github.com>
2021-12-20 19:44:56 +1300

    fix(upload):support upload big file ee-2060 (#1084)
    * fix/ee-2060/support-upload-huge-file

--- api/http/utils/request.go
+++ api/http/utils/request.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 )
@@ -13,7 +14,11 @@ func CopyRequestBody(r *http.Request) []byte {
 	if r.Body == nil {
 		return nil
 	}
-
+	//for upload images and files, skip log the body
+	if (strings.Contains(r.URL.Path, "browse/put") && r.Method == "POST") ||
+		(strings.Contains(r.URL.Path, "images/load") && r.Method == "POST") {
+		return nil
+	}
 	// the implementation is a bit naive as we intend to read the whole body in-memory
 	// that might be problematic in case of large payloads, but in a general case shouldn't be a problem
 	body, err := io.ReadAll(r.Body)
