a3b3833ad00a8f529dadc9ec9420563a6ae4eb94
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-02-01 09:37:09 +0200

    fix(agent): add agent header [EE-2433] (#1172)
    * fix(agent): add agent header [EE-2433]
    
    fix [EE-2433]
    
    * fix(containers): reload current endpoint id

--- app/docker/services/containerService.js
+++ app/docker/services/containerService.js
@@ -200,8 +200,10 @@ function ContainerServiceFactory($q, Container, LogHelper, $timeout, EndpointPro
   return service;
 
   function withEndpointId(func) {
-    const endpointId = EndpointProvider.endpointID();
+    return function (...args) {
+      const endpointId = EndpointProvider.endpointID();
 
-    return func.bind(null, endpointId);
+      return func(endpointId, ...args);
+    };
   }
 }
