db112962cb90225f2c24b0f170b63ad82d484f2b
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-07-06 11:23:51 +0300

    refactor(app): redesign dashboard-item component [EE-3634] (#1856)

--- app/nomad/Dashboard/DashboardView.tsx
+++ app/nomad/Dashboard/DashboardView.tsx
@@ -6,6 +6,7 @@ import { useDashboard } from '@/nomad/hooks/useDashboard';
 import { DashboardItem } from '@@/DashboardItem';
 import { Widget, WidgetTitle, WidgetBody } from '@@/Widget';
 import { PageHeader } from '@@/PageHeader';
+import { DashboardGrid } from '@@/DashboardItem/DashboardGrid';
 
 import { RunningStatus } from './RunningStatus';
 
@@ -29,52 +30,55 @@ export function DashboardView() {
           <i className="fa fa-cog fa-spin space-left" />
         </div>
       ) : (
-        <div className="row">
-          <div className="col-sm-12">
-            {/* cluster info */}
-            <Widget>
-              <WidgetTitle
-                icon="fa-tachometer-alt"
-                title="Cluster information"
-              />
-              <WidgetBody className="no-padding">
-                <table className="table">
-                  <tbody>
-                    <tr>
-                      <td>Nodes in the cluster</td>
-                      <td>{dashboardQuery.data?.NodeCount ?? '-'}</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </WidgetBody>
-            </Widget>
-          </div>
+        <>
           <div className="row">
-            {/* jobs */}
-            <DashboardItem
-              value={dashboardQuery.data?.JobCount}
-              icon="fa fa-th-list"
-              type="Nomad Jobs"
-            />
-
-            {/* groups */}
-            <DashboardItem
-              value={dashboardQuery.data?.GroupCount}
-              icon="fa fa-list-alt"
-              type="Groups"
-            />
-
-            {/* tasks */}
-            <DashboardItem
-              value={dashboardQuery.data?.TaskCount}
-              icon="fa fa-cubes"
-              type="Tasks"
-            >
-              {/* running status of tasks */}
-              <RunningStatus running={running} stopped={stopped} />
-            </DashboardItem>
+            <div className="col-sm-12">
+              {/* cluster info */}
+              <Widget>
+                <WidgetTitle
+                  icon="fa-tachometer-alt"
+                  title="Cluster information"
+                />
+                <WidgetBody className="no-padding">
+                  <table className="table">
+                    <tbody>
+                      <tr>
+                        <td>Nodes in the cluster</td>
+                        <td>{dashboardQuery.data?.NodeCount ?? '-'}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </WidgetBody>
+              </Widget>
+            </div>
           </div>
-        </div>
+
+          <div className="mx-4">
+            <DashboardGrid>
+              {/* jobs */}
+              <DashboardItem
+                value={dashboardQuery.data?.JobCount}
+                icon="fa fa-th-list"
+                type="Nomad Job"
+              />
+              {/* groups */}
+              <DashboardItem
+                value={dashboardQuery.data?.GroupCount}
+                icon="fa fa-list-alt"
+                type="Group"
+              />
+              {/* tasks */}
+              <DashboardItem
+                value={dashboardQuery.data?.TaskCount}
+                icon="fa fa-cubes"
+                type="Task"
+              >
+                {/* running status of tasks */}
+                <RunningStatus running={running} stopped={stopped} />
+              </DashboardItem>
+            </DashboardGrid>
+          </div>
+        </>
       )}
     </>
   );
