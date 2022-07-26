399260b1a97f4b4daf050997d1b8ceb8abdf9f91
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-07-26 19:00:01 +0200

    refactor(azure): migrate module to react [EE-2782] (#1354)
    * refactor(azure): migrate module to react [EE-2782]
    
    * fix(azure): remove optional chain
    
    * feat(azure): apply new icons in dashboard
    
    * feat(azure): apply new icons in dashboard
    
    * feat(ui): allow single string for breadcrumbs
    
    * refactor(azure/containers): use Table.content
    
    * feat(azure/containers): implement new ui [EE-3538]
    
    * fix(azure/containers): use correct icon
    
    * chore(tests): mock svg as component
    
    * fix(azure): fix tests
    
    Co-authored-by: matias-portainer <matias.spinarolli@portainer.io>

--- app/portainer/environments/index.ts
+++ app/portainer/environments/index.ts
@@ -1,5 +1,8 @@
 import angular from 'angular';
 
 import kaasModule from './kaas';
+import { azureEndpointConfig } from './azure-endpoint-config/azure-endpoint-config';
 
-export default angular.module('portainer.environments', [kaasModule]).name;
+export default angular
+  .module('portainer.environments', [kaasModule])
+  .component('azureEndpointConfig', azureEndpointConfig).name;
