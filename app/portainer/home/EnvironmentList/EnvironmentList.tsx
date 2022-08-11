4207bef72b1c2e46bcf1f3b61beaf8a457a040bb
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2022-08-12 02:00:28 +0300

    fix(home): clear all filters [EE-3912] (#2099)

--- app/portainer/home/EnvironmentList/EnvironmentList.tsx
+++ app/portainer/home/EnvironmentList/EnvironmentList.tsx
@@ -456,6 +456,8 @@ export function EnvironmentList({ onClickItem, onRefresh }: Props) {
     setTagFilter([]);
     setGroupState([]);
     setGroupFilter([]);
+    setAgentVersions([]);
+    setConnectionTypes([]);
   }
 
   function sortOnchange(filterOptions: Filter) {
