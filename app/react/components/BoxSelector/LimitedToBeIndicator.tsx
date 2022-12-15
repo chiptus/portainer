a0d75dc4ba423f9b1ec39c6e59ca931a7ac89f96
matias-portainer <104775949+matias-portainer@users.noreply.github.com>
2022-12-15 11:59:10 -0300

    fix(wording): change wording on CE teaser EE-4590 (#2574)

--- app/react/components/BoxSelector/LimitedToBeIndicator.tsx
+++ app/react/components/BoxSelector/LimitedToBeIndicator.tsx
@@ -11,7 +11,7 @@ export function LimitedToBeIndicator({ tooltipId }: Props) {
   return (
     <div className="absolute left-0 top-0 w-full">
       <div className="mx-auto max-w-fit bg-warning-4 rounded-b-lg py-1 px-3 flex gap-1 text-sm items-center">
-        <span className="text-warning-9">Pro Feature</span>
+        <span className="text-warning-9">BE Feature</span>
         <TooltipWithChildren
           position="bottom"
           className={clsx(tooltipId, 'portainer-tooltip')}
