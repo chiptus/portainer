b93c3bfe87d07cf65d70347e44cae53d85b35998
LP B <xAt0mZ@users.noreply.github.com>
2022-12-15 17:49:46 +0100

    fix(app): cursor jumps to the end of inputs [EE-4359] (#2580)
    * feat(app/react): introduce controlled inputs HOC to avoid creating uncontrolled React components
    
    * fix(app/env): jumping inputs when adding gpus to existing environment

--- app/portainer/components/forms/git-form/index.js
+++ app/portainer/components/forms/git-form/index.js
@@ -7,6 +7,7 @@ import { GitFormRefField } from '@/react/portainer/gitops/GitFormRefField';
 import { withCurrentUser } from '@/react-tools/withCurrentUser';
 import { withReactQuery } from '@/react-tools/withReactQuery';
 import { withUIRouter } from '@/react-tools/withUIRouter';
+import { withControlledInput } from '@/react-tools/withControlledInput';
 import { GitFormAuthFieldset } from '@/portainer/components/forms/git-form/git-form-auth-fieldset/GitFormAuthFieldset';
 import { gitForm } from './git-form';
 import { gitFormAdditionalFilesPanel } from './git-form-additional-files-panel';
@@ -23,31 +24,44 @@ export default angular
   .component('gitFormAutoUpdateFieldset', gitFormAutoUpdateFieldset)
   .component(
     'gitFormComposePathField',
-    r2a(withUIRouter(withReactQuery(withCurrentUser(GitFormComposePathField))), ['value', 'onChange', 'isCompose', 'model', 'isDockerStandalone'])
+    r2a(withUIRouter(withReactQuery(withCurrentUser(withControlledInput(GitFormComposePathField)))), ['value', 'onChange', 'isCompose', 'model', 'isDockerStandalone'])
   )
-  .component('gitFormRefField', r2a(withUIRouter(withReactQuery(withCurrentUser(GitFormRefField))), ['value', 'onChange', 'model']))
+  .component('gitFormRefField', r2a(withUIRouter(withReactQuery(withCurrentUser(withControlledInput(GitFormRefField)))), ['value', 'onChange', 'model']))
   .component(
     'gitFormUrlField',
-    r2a(withUIRouter(withReactQuery(withCurrentUser(GitFormUrlField))), ['value', 'onChange', 'onChangeRepositoryValid', 'onRefreshGitopsCache', 'model'])
+    r2a(withUIRouter(withReactQuery(withCurrentUser(withControlledInput(GitFormUrlField)))), ['value', 'onChange', 'onChangeRepositoryValid', 'onRefreshGitopsCache', 'model'])
   )
   .component(
     'gitFormAuthFieldset',
-    r2a(withUIRouter(withReactQuery(withCurrentUser(GitFormAuthFieldset))), [
-      'repositoryAuthentication',
-      'repositoryUsername',
-      'repositoryPassword',
-      'gitCredentials',
-      'selectedGitCredential',
-      'saveCredential',
-      'showAuthExplanation',
-      'newCredentialName',
-      'newCredentialNameExist',
-      'newCredentialNameInvalid',
-      'onSelectGitCredential',
-      'onChangeRepositoryAuthentication',
-      'onChangeRepositoryUsername',
-      'onChangeRepositoryPassword',
-      'onChangeSaveCredential',
-      'onChangeNewCredentialName',
-    ])
+    r2a(
+      withUIRouter(
+        withReactQuery(
+          withCurrentUser(
+            withControlledInput(GitFormAuthFieldset, {
+              repositoryUsername: 'onChangeRepositoryUsername',
+              repositoryPassword: 'onChangeRepositoryPassword',
+              newCredentialName: 'onChangeNewCredentialName',
+            })
+          )
+        )
+      ),
+      [
+        'repositoryAuthentication',
+        'repositoryUsername',
+        'repositoryPassword',
+        'gitCredentials',
+        'selectedGitCredential',
+        'saveCredential',
+        'showAuthExplanation',
+        'newCredentialName',
+        'newCredentialNameExist',
+        'newCredentialNameInvalid',
+        'onSelectGitCredential',
+        'onChangeRepositoryAuthentication',
+        'onChangeRepositoryUsername',
+        'onChangeRepositoryPassword',
+        'onChangeSaveCredential',
+        'onChangeNewCredentialName',
+      ]
+    )
   ).name;
