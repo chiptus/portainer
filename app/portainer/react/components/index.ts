fdef2a988c94779140932fc76b980e5321228597
Chamhaw <chamhaw@gmail.com>
2023-05-25 11:59:45 +0800

    fix(service): service related UI issues [EE-4062] (#2488)

--- app/portainer/react/components/index.ts
+++ app/portainer/react/components/index.ts
@@ -86,6 +86,7 @@ export const componentsModule = angular
       'disabled',
       'dataCy',
       'onChange',
+      'index',
       'featureId',
       'className',
     ])
--- app/react/components/form-components/SwitchField/SwitchField.tsx
+++ app/react/components/form-components/SwitchField/SwitchField.tsx
@@ -11,8 +11,9 @@ import { Switch } from './Switch';
 export interface Props {
   label: string;
   checked: boolean;
-  onChange(value: boolean): void;
+  onChange(value: boolean, index?: number): void;
 
+  index?: number;
   name?: string;
   tooltip?: ComponentProps<typeof Tooltip>['message'];
   setTooltipHtmlMessage?: ComponentProps<typeof Tooltip>['setHtmlMessage'];
@@ -28,6 +29,7 @@ export function SwitchField({
   tooltip,
   checked,
   label,
+  index,
   name,
   labelClass,
   fieldClass,
@@ -39,7 +41,6 @@ export function SwitchField({
   setTooltipHtmlMessage,
 }: Props) {
   const toggleName = name ? `toggle_${name}` : '';
-
   return (
     <label className={clsx(styles.root, fieldClass)}>
       <span
@@ -58,6 +59,7 @@ export function SwitchField({
         disabled={disabled}
         onChange={onChange}
         featureId={featureId}
+        index={index}
         dataCy={dataCy}
       />
     </label>
