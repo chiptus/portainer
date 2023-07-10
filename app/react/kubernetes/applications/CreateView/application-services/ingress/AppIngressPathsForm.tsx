115356751eb84a93f8800cc6fae256ef5a77f433
Ali <83188384+testA113@users.noreply.github.com>
2023-07-10 16:20:15 +1200

    fix(app): fix app ingress edge cases [EE-5663]  (#3428)

--- app/react/kubernetes/applications/CreateView/application-services/ingress/AppIngressPathsForm.tsx
+++ app/react/kubernetes/applications/CreateView/application-services/ingress/AppIngressPathsForm.tsx
@@ -1,4 +1,4 @@
-import { Plus } from 'lucide-react';
+import { Loader2, Plus } from 'lucide-react';
 import { FormikErrors } from 'formik';
 import { useMemo } from 'react';
 
@@ -15,6 +15,7 @@ import { Button } from '@@/buttons';
 import { SwitchField } from '@@/form-components/SwitchField';
 import { TextTip } from '@@/Tip/TextTip';
 import { Link } from '@@/Link';
+import { Icon } from '@@/Icon';
 
 import { ServicePortIngressPath } from '../types';
 
@@ -88,7 +89,12 @@ export function AppIngressPathsForm({
   }
 
   if (ingressesQuery.isLoading || ingressControllersQuery.isLoading) {
-    return <p>Loading ingresses...</p>;
+    return (
+      <p className="text-muted mt-2 flex items-center gap-x-2 text-sm">
+        <Icon icon={Loader2} className="animate-spin-slow" />
+        Loading ingresses...
+      </p>
+    );
   }
 
   if (!isAdmin && restrictAccessToIngress) {
