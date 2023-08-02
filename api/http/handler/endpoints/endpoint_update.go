b169021c204c1bd6f29db6b3086464b3e126797b
matias-portainer <104775949+matias-portainer@users.noreply.github.com>
2023-08-02 11:06:49 -0300

    fix(endpoints): fix nil pointer dereference EE-5843 (#3620)

--- api/http/handler/endpoints/endpoint_update.go
+++ api/http/handler/endpoints/endpoint_update.go
@@ -403,7 +403,7 @@ func shouldReloadTLSConfiguration(endpoint *portaineree.Endpoint, payload *endpo
 	// When updating Docker API environment, as long as TLS is true and TLSSkipVerify is false,
 	// we assume that new TLS files have been uploaded and we need to reload the TLS configuration.
 	if endpoint.Type != portaineree.DockerEnvironment ||
-		!strings.HasPrefix(*payload.URL, "tcp://") ||
+		(payload.URL != nil && !strings.HasPrefix(*payload.URL, "tcp://")) ||
 		payload.TLS == nil || !*payload.TLS {
 		return false
 	}
