4f00cdaba769569628b4567df5e04632c6c30492
Oscar Zhou <100548325+oscarzhou-portainer@users.noreply.github.com>
2023-08-07 10:30:44 +1200

    fix(react/datatable): override getColumnCanGlobalFilter method (#3656)

--- app/react/components/datatables/Datatable.tsx
+++ app/react/components/datatables/Datatable.tsx
@@ -3,6 +3,7 @@ import {
   TableState,
   useReactTable,
   Row,
+  Column,
   getCoreRowModel,
   getPaginationRowModel,
   getFilteredRowModel,
@@ -132,6 +133,7 @@ export function Datatable<
     getFacetedMinMaxValues: getFacetedMinMaxValues(),
     getExpandedRowModel: getExpandedRowModel(),
     getRowCanExpand,
+    getColumnCanGlobalFilter,
     ...(isServerSidePagination ? { manualPagination: true, pageCount } : {}),
     meta,
   });
@@ -259,4 +261,11 @@ function globalFilterFn<D>(
   }
 
   return false;
+}
+
+function getColumnCanGlobalFilter<D>(column: Column<D, unknown>): boolean {
+  if (column.id === 'select') {
+    return false;
+  }
+  return true;
 }
