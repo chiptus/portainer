d5eb0ab12d969868878dbac42748540ec9e484ca
cmeng <cmenginnz@gmail.com>
2023-08-16 10:22:59 +1200

    fix(edge-config): add text tip for edge config EE-5959 (#3771)

--- app/react/edge/edge-configurations/common/EdgeConfigurationTypeSelectorField.tsx
+++ app/react/edge/edge-configurations/common/EdgeConfigurationTypeSelectorField.tsx
@@ -3,6 +3,7 @@ import { useField } from 'formik';
 
 import { BoxSelector } from '@@/BoxSelector';
 import { BoxSelectorOption } from '@@/BoxSelector/types';
+import { TextTip } from '@@/Tip/TextTip';
 
 import { FormValues, FormValuesEdgeConfigurationType } from './types';
 import { DeviceSpecificConfigurationFields } from './DeviceSpecificConfigurationFields';
@@ -31,9 +32,31 @@ export function EdgeConfigurationTypeSelectorField() {
   const [{ value: configurationType }, , { setValue }] =
     useField<FormValues['type']>('type');
 
+  const textTipForGeneral =
+    'Selecting this option will result in the uploaded configuration ' +
+    'being sent to every edge device. The configuration will be placed in the defined directory of ' +
+    'each edge device. This choice ensures uniformity across all devices, as they will share the ' +
+    'same configuration.';
+  const textTipForDeviceSpecific =
+    'Opting for this approach means that only ' +
+    'configurations matching specific criteria will be transmitted to certain edge devices. The ' +
+    'uploaded configuration will be placed in the defined directory of only those devices that ' +
+    'meet the criteria. This offers targeted customization, tailoring configurations to the ' +
+    'unique requirements of individual devices.';
+  const textTip =
+    configurationType === FormValuesEdgeConfigurationType.General
+      ? textTipForGeneral
+      : textTipForDeviceSpecific;
+
   return (
     <>
-      <div className="col-sm-12 form-section-title">Type</div>
+      <div className="col-sm-12 form-section-title">
+        <div>Type</div>
+        <TextTip color="blue" inline={false} className="mt-2">
+          {textTip}
+        </TextTip>
+      </div>
+
       <BoxSelector
         radioName="configurationType"
         value={configurationType}
