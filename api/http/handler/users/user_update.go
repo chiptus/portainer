8d226533b2b66f97edcce5453c7974ea7c3b1131
Matt Hook <hookenz@gmail.com>
2023-09-06 08:46:46 +1200

    fix(users): prevent regular users changing their username [EE-5828] (#3702)

--- api/http/handler/users/user_update.go
+++ api/http/handler/users/user_update.go
@@ -47,6 +47,7 @@ func (payload *userUpdatePayload) Validate(r *http.Request) error {
 // @id UserUpdate
 // @summary Update a user
 // @description Update user details. A regular user account can only update his details.
+// @description A regular user account cannot change their username or role.
 // @description **Access policy**: authenticated
 // @tags users
 // @security ApiKeyAuth
@@ -99,6 +100,10 @@ func (handler *Handler) userUpdate(w http.ResponseWriter, r *http.Request) *http
 	}
 
 	if payload.Username != "" && payload.Username != user.Username {
+		if tokenData.Role != portaineree.AdministratorRole {
+			return httperror.Forbidden("Permission denied. Unable to update username", httperrors.ErrResourceAccessDenied)
+		}
+
 		sameNameUser, err := handler.DataStore.User().UserByUsername(payload.Username)
 		if err != nil && !handler.DataStore.IsErrObjectNotFound(err) {
 			return httperror.InternalServerError("Unable to retrieve users from the database", err)
