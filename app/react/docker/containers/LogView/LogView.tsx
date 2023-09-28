39776fae0059a93f6f9b92cf265a1258db0750d6
LP B <xAt0mZ@users.noreply.github.com>
2023-09-28 15:53:55 +0200

    fix(docker/container): container logs viewer error when logging is disabled (#4027)
    * fix(docker/container): let docker select the logging driver by default on container create
    
    * fix(docker/container-logs): information panel in container logs when logging is disabled
    
    * fix(docker/container): dont include HostConfig.LogConfig if no driver is selected
    
    * refactor(docker/container-log): use ternary to display LogViewer

--- app/react/docker/containers/LogView/LogView.tsx
+++ app/react/docker/containers/LogView/LogView.tsx
@@ -9,6 +9,9 @@ import { ContainerLogsParams } from '@/react/docker/containers/types';
 import { PageHeader } from '@@/PageHeader';
 import { LogViewer } from '@@/LogViewer';
 import { GetLogsParamsInterface } from '@@/LogViewer/types';
+import { InformationPanel } from '@@/InformationPanel';
+import { TextTip } from '@@/Tip/TextTip';
+import { Link } from '@@/Link';
 
 export function LogView() {
   const {
@@ -33,7 +36,13 @@ export function LogView() {
   );
 
   const containerQuery = useContainer(environmentId, containerId);
-  const containerName = containerQuery.data?.Name?.substring(1) || '';
+  if (!containerQuery.data || containerQuery.isLoading) {
+    return null;
+  }
+  const containerName = containerQuery.data.Name?.substring(1) || '';
+  const logsEnabled =
+    containerQuery.data.HostConfig?.LogConfig?.Type && // if a portion of the object path doesn't exist, logging is likely disabled
+    containerQuery.data.HostConfig.LogConfig.Type !== 'none'; // if type === none logging is disabled
   const breadcrumbs = [
     { label: 'Containers', link: 'docker.containers' },
     {
@@ -47,11 +56,38 @@ export function LogView() {
   return (
     <>
       <PageHeader title="Container logs" breadcrumbs={breadcrumbs} />
-      <LogViewer
-        resourceType="docker-container"
-        resourceName={containerName}
-        getLogsFn={getLogsFn}
-      />
+
+      {logsEnabled ? (
+        <LogViewer
+          resourceType="docker-container"
+          resourceName={containerName}
+          getLogsFn={getLogsFn}
+        />
+      ) : (
+        <LogsDisabledInfoPanel />
+      )}
     </>
+  );
+}
+
+function LogsDisabledInfoPanel() {
+  const {
+    params: { id: containerId, nodeName },
+  } = useCurrentStateAndParams();
+
+  return (
+    <InformationPanel>
+      <TextTip color="blue">
+        Logging is disabled for this container. If you want to re-enable
+        logging, please{' '}
+        <Link
+          to="docker.containers.new"
+          params={{ from: containerId, nodeName }}
+        >
+          redeploy your container
+        </Link>{' '}
+        and select a logging driver in the &quot;Command & logging&quot; panel.
+      </TextTip>
+    </InformationPanel>
   );
 }
