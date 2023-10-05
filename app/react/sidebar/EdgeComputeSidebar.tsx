bef4ae78139d8b00b3b1f091b4dbb5c1b0a6ba14
Chaim Lev-Ari <chiptus@users.noreply.github.com>
2023-10-05 10:31:12 +0300

    fix(edge/waitingroom): hide sidebar when disabled [EE-6003] (#3995)

--- app/react/sidebar/EdgeComputeSidebar.tsx
+++ app/react/sidebar/EdgeComputeSidebar.tsx
@@ -1,11 +1,21 @@
 import { Box, Clock, LayoutGrid, Layers, Puzzle } from 'lucide-react';
 
 import { isBE } from '../portainer/feature-flags/feature-flags.service';
+import { useSettings } from '../portainer/settings/queries';
 
 import { SidebarItem } from './SidebarItem';
 import { SidebarSection } from './SidebarSection';
 
 export function EdgeComputeSidebar() {
+  // this sidebar is rendered only for admins, so we can safely assume that settingsQuery will succeed
+  const settingsQuery = useSettings();
+
+  if (!settingsQuery.data || !settingsQuery.data.EnableEdgeComputeFeatures) {
+    return null;
+  }
+
+  const settings = settingsQuery.data;
+
   return (
     <SidebarSection title="Edge compute">
       <SidebarItem
@@ -34,7 +44,7 @@ export function EdgeComputeSidebar() {
           data-cy="portainerSidebar-edgeConfigurations"
         />
       )}
-      {isBE && (
+      {isBE && !settings.TrustOnFirstConnect && (
         <SidebarItem
           to="edge.devices.waiting-room"
           label="Waiting Room"
