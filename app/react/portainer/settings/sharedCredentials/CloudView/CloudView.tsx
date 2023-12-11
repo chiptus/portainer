a2c4239dff55e1f3e9f8bec12248488b96d11d2a
Dakota Walsh <101994734+dakota-portainer@users.noreply.github.com>
2023-12-12 11:52:14 +1300

    fix(access controls): redirect non-admins to home for cloud/settings EE-5861 (#4147)

--- app/react/portainer/settings/sharedCredentials/CloudView/CloudView.tsx
+++ app/react/portainer/settings/sharedCredentials/CloudView/CloudView.tsx
@@ -1,8 +1,22 @@
+import { useRouter } from '@uirouter/react';
+import { useEffect } from 'react';
+
+import { useCurrentUser } from '@/react/hooks/useUser';
+
 import { PageHeader } from '@@/PageHeader';
 
 import { CredentialsDatatable } from './CloudCredentialsDatatable/CredentialsDatatable';
 
 export function CloudView() {
+  const { isAdmin } = useCurrentUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAdmin) {
+      router.stateService.go('portainer.home');
+    }
+  }, [isAdmin, router.stateService]);
+
   return (
     <>
       <PageHeader
